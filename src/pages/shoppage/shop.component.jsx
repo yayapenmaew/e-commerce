d41946ae2d671088a097e663db221e9f72370285
Fungtammasan <sfungtammasa@c02c91fnlvdm.agoda.local>
2021-12-22 10:40:51 +0700

    add header component

--- src/pages/shoppage/shop.component.jsx
+++ src/pages/shoppage/shop.component.jsx
@@ -15,7 +15,6 @@ class ShopPage extends React.Component {
     const { collections } = this.state;
     return (
       <div>
-        SHOP PAGE
         {collections.map(({ id, ...otherCollectionProps }) => (
           <CollectionPreview key={id} {...otherCollectionProps} />
         ))}
