82c8e39588b4dca23dc53c7a5a30ede8aefd4bfb
Fungtammasan <sfungtammasa@c02c91fnlvdm.agoda.local>
2021-12-22 17:41:53 +0700

    finishing sign in and sign up

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shoppage/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from "./firebase/firebase.util";
+import {
+  auth,
+  createUserProfileDocument,
+  firestore,
+} from "./firebase/firebase.util";
 
 class App extends React.Component {
   constructor() {
@@ -18,9 +22,24 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState(
+            {
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data(),
+              },
+            },
+            () => console.log(this.state)
+          );
+        });
+        console.log(this.state);
+      }
+      this.setState({ currentUser: userAuth });
     });
   }
 
